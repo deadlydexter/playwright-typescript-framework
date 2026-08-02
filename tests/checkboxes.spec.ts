bab1ecd166a4a42bb53bb64a1ce2354fee4e9799
satheesh swaminathan <32887710+deadlydexter@users.noreply.github.com>
2026-08-01 21:51:42 -0400

    Add test tags and suite selection

--- tests/checkboxes.spec.ts
+++ tests/checkboxes.spec.ts
@@ -1,11 +1,17 @@
 import { test } from '../fixtures/testFixtures';
 
 test.describe('Checkbox tests', () => {
-  test('select and clear checkboxes', async ({ checkboxesPage }) => {
-    await checkboxesPage.navigate();
-    await checkboxesPage.verifyPageIsDisplayed();
-    await checkboxesPage.verifyInitialStates();
-    await checkboxesPage.checkFirstCheckbox();
-    await checkboxesPage.uncheckSecondCheckbox();
-  });
+  test(
+    'select and clear checkboxes',
+    {
+      tag: ['@smoke', '@regression'],
+    },
+    async ({ checkboxesPage }) => {
+      await checkboxesPage.navigate();
+      await checkboxesPage.verifyPageIsDisplayed();
+      await checkboxesPage.verifyInitialStates();
+      await checkboxesPage.checkFirstCheckbox();
+      await checkboxesPage.uncheckSecondCheckbox();
+    }
+  );
 });
--- tests/dropdown.spec.ts
+++ tests/dropdown.spec.ts
@@ -1,15 +1,21 @@
 import { test } from '../fixtures/testFixtures';
 
 test.describe('Dropdown tests', () => {
-  test('select dropdown options by label and value', async ({ dropdownPage }) => {
-    await dropdownPage.navigate();
-    await dropdownPage.verifyPageIsDisplayed();
-    await dropdownPage.verifyDefaultOption();
+  test(
+    'select dropdown options by label and value',
+    {
+      tag: '@regression',
+    },
+    async ({ dropdownPage }) => {
+      await dropdownPage.navigate();
+      await dropdownPage.verifyPageIsDisplayed();
+      await dropdownPage.verifyDefaultOption();
 
-    await dropdownPage.selectOptionByLabel('Option 1');
-    await dropdownPage.verifySelectedValue('1');
+      await dropdownPage.selectOptionByLabel('Option 1');
+      await dropdownPage.verifySelectedValue('1');
 
-    await dropdownPage.selectOptionByValue('2');
-    await dropdownPage.verifySelectedValue('2');
-  });
+      await dropdownPage.selectOptionByValue('2');
+      await dropdownPage.verifySelectedValue('2');
+    }
+  );
 });
